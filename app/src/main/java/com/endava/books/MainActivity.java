e0d28a519e9df092afd78d2d953a4d9b699c6d22
Jhonnyguzz <jhjguzmanri@unal.edu.co>
2019-05-10 10:45:02 -0500

    Adding Intent for second activity

--- app/src/main/java/com/endava/books/MainActivity.java
+++ app/src/main/java/com/endava/books/MainActivity.java
@@ -2,13 +2,38 @@ package com.endava.books;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.endava.books.view.ListBookView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonEnter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        //buttonEnter = findViewById(R.id.enterButton);
+
+        /*
+        buttonEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent listBookList = new Intent(getApplicationContext(), ListBookView.class);
+                startActivity(listBookList);
+            }
+        });
+        */
     }
+
+    public void showListBook(View view) {
+        Intent listBookList = new Intent(this, ListBookView.class);
+        startActivity(listBookList);
+    }
+
 }
